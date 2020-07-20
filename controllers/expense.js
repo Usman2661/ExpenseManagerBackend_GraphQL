15086833eec964347389373bd2e34c4d0b0205d6
Usman Ali <uali@modern-networks.co.uk>
2020-07-20 18:16:51 +0100

    upload logo for company

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,5 +1,3 @@
-const ExpenseReceipt = require('../models').ExpenseReceipt;
-
 exports.createExpense = async (req, res, next) => {
   if (req.files.length > 0) {
     try {
@@ -17,6 +15,33 @@ exports.createExpense = async (req, res, next) => {
       res.status(201).json({
         message: 'Reciepts Added!',
         data: expenses,
+      });
+    } catch (error) {
+      res.status(500).json({
+        error: error,
+      });
+    }
+  } else {
+    res.status(404).json({
+      message: 'Not Files Found!',
+    });
+  }
+};
+
+exports.uploadLogo = async (req, res, next) => {
+  if (req.files.length > 0) {
+    try {
+      const companyLogo = req.files.map(async (file) => {
+        const logo = {
+          logo: 'http://localhost:3001/images/userExpenses/' + file.filename,
+        };
+
+        return logo;
+      });
+      const logo = await Promise.all(companyLogo);
+      res.status(201).json({
+        message: 'Logo Uploaded!',
+        data: logo,
       });
     } catch (error) {
       res.status(500).json({
--- routes/expense.js
+++ routes/expense.js
@@ -4,5 +4,6 @@ const ExpenseController = require('../controllers/expense');
 const uploadReceipts = require('../middleware/multer');
 
 router.post('/', uploadReceipts.saveReciepts, ExpenseController.createExpense);
+router.post('/logo', uploadReceipts.saveReciepts, ExpenseController.uploadLogo);
 
 module.exports = router;
